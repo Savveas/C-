2152f6e7417c7915f5843135cf5cb5564e7e9599
Savvas Petridis <115417618+Savveas@users.noreply.github.com>
2024-02-29 00:21:01 +0200

    Add files via upload

--- TMVClassificationApplication.C
+++ TMVClassificationApplication.C
@@ -9,25 +9,29 @@ using namespace TMVA;
 
 void TMVClassificationApplication() {
 
-    TFile *input_Signal = TFile::Open("histos_signal.root");
-    TTree* sigTree = (TTree*)input_Signal->Get( "my_tree" );
-
-    //TFile *input_TTbarSemileptonic = TFile::Open("histos_back_TTbarSemileptonic.root");
-    //TTree* bkgTree_Semileptonic = (TTree*)input_TTbarSemileptonic->Get( "my_tree" );
-
-    //TFile *input_TTbarDileptonic = TFile::Open("histos_back_TTbarDileptonic.root");
-    //TTree* bkgTree_Dileptonic = (TTree*)input_TTbarDileptonic->Get( "my_tree" );
-
-    //TFile *input_TTbarHadronic = TFile::Open("histos_back_TTbarHadronic.root");
-    //TTree* bkgTree_Hadronic = (TTree*)input_TTbarHadronic->Get( "my_tree" );
-
-    //TFile *input_WJetsToLNu = TFile::Open("histos_back_WJetsToLNu.root");
-    //TTree* bkgTree_W = (TTree*)input_WJetsToLNu->Get( "my_tree" );
-
+    
+    TH1F *BDT_eval = new TH1F("BDT_eval","BDT_eval",100,0.,100.);
     TMVA::Reader *reader = new TMVA::Reader("!Color"); 
+    Float_t mt;
+    Float_t inv_m;
+    Float_t met_pt;
+    Float_t btag_0;
+    Float_t btag_1;
+    Float_t btag_2;
+    Float_t btag_3;
+    Float_t lep_pt;
+    Float_t Nbjets_after;
+    Float_t w_pt;
+    Float_t d_phi_w_h;
+    Float_t Njets_after;
+    Float_t dR_av;
+    Float_t minDelta_m;
+    Float_t d_phi_j_E;
+    Float_t h_pt;
+    Float_t H_T;
 
-    Float_t h_pt mt inv_m met_pt btag_0 btag_1 btag_2 lep_pt Nbjets_after w_pt d_phi_w_h w_pt dR_av minDelta_m d_phi_j_E H_T
-    Int_t Nbjets_after Njets_after 
+
+
 
     reader->AddVariable( "h_pt", &h_pt );
     reader->AddVariable( "mt", &mt );
@@ -49,27 +53,36 @@ void TMVClassificationApplication() {
 
     reader->BookMVA("kBDT method","MVAnalysis/weights/MVAnalysis_BDT.weights.xml");
 
+    TFile *input_Signal = TFile::Open("histos_signal.root");
+    TTree* sigTree = (TTree*)input_Signal->Get( "my_tree" );
 
-    //if (fChain == 0) return;
+    //TFile *input_TTbarSemileptonic = TFile::Open("histos_back_TTbarSemileptonic.root");
+    //TTree* bkgTree_Semileptonic = (TTree*)input_TTbarSemileptonic->Get( "my_tree" );
+
+    //TFile *input_TTbarDileptonic = TFile::Open("histos_back_TTbarDileptonic.root");
+    //TTree* bkgTree_Dileptonic = (TTree*)input_TTbarDileptonic->Get( "my_tree" );
+
+    //TFile *input_TTbarHadronic = TFile::Open("histos_back_TTbarHadronic.root");
+    //TTree* bkgTree_Hadronic = (TTree*)input_TTbarHadronic->Get( "my_tree" );
+
+    //TFile *input_WJetsToLNu = TFile::Open("histos_back_WJetsToLNu.root");
+    //TTree* bkgTree_W = (TTree*)input_WJetsToLNu->Get( "my_tree" );
 
 
-
-  Long64_t nentries = fChain->GetEntriesFast();
-  or (Long64_t jentry=0; jentry<nentries;jentry++) {
+for (Long64_t ievt=3000; ievt<sigTree->GetEntries();ievt++) {
+sigTree->GetEntry(ievt);
+  
     
-    TH1F *BDT_eval = new TH1F("BDT_eval","BDT_eval",100,0.,100.);
+    
     
     Double_t out = reader->EvaluateMVA( "kBDT method" );
     BDT_eval->Fill(out);
+}
     TCanvas *c_BDT_eval = new TCanvas("BDT_eval", "BDT_eval", 1000, 1000);
   BDT_eval->GetXaxis()->SetTitle("BDT_eval");
   BDT_eval->GetYaxis()->SetTitle("Entries");
   BDT_eval->SetLineColor(kBlack);
   BDT_eval->Scale(1./BDT_eval->Integral());
   BDT_eval->Draw("Ehist");
-
-
-  }
-  BDT_eval->Write();
-
 }
+
